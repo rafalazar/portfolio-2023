23621111ec3deeff2b11ceab032809dc6aaa77f4
Rafael Salazar <jcr.salazar@outlook.com>
2022-12-06 17:02:57 -0500

    feat: finish skills component, but it can be refactored

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,14 +1,15 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const Skills = () => {
   return (
     <div className='w-full lg:h-screen p-2'>
       <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Skills</p>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Skills
+        </p>
         <h2 className='py-4'>What I Can Do</h2>
         <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
@@ -136,11 +137,10 @@ const Skills = () => {
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
