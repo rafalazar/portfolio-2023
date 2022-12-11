4b19b0c1cd848616f32542def44de746af477ac7
Rafael Salazar <jcr.salazar@outlook.com>
2022-12-11 18:44:48 -0500

    feat: remove Navbar unnecessary import

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Main from '../components/Main';
-import Navbar from '../components/Navbar';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 
