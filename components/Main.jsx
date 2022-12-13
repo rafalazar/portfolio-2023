39c0490cb5f089732a9696a45c8671de44ce93ba
Rafael Salazar <jcr.salazar@outlook.com>
2022-12-12 20:23:53 -0500

    feat: update Main component
    Add typed js in order to add animation to the text.
    It needs refactor.

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
+import Typed from 'typed.js';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const Main = () => {
+  const prueba = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(prueba.current, {
+      strings: ['A Front-End Developer', 'A Back-End Developer', 'A Full-Stack Developer'],
+      startDelay: 3000,
+      typeSpeed: 80,
+      backSpeed: 100,
+      backDelay: 100
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
@@ -14,7 +31,7 @@ const Main = () => {
           <h1 className='py-4 text-gray-700'>
             Hi, I&apos;m <span className='text-[#5651e5]'>Jordan</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A Full-Stack Web Developer</h1>
+          <h1 ref={prueba} className='py-2 text-gray-700'></h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
             I&apos;m a full-stack web developer specializing in building (and
             occasionally designing) exceptional digital experiences. Currently
