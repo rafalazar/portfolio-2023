32b39116448bebe5e638fe95d204f44b8793efb7
Rafael Salazar <jcr.salazar@outlook.com>
2022-12-15 16:08:12 -0500

    feat: update Main component
    Add new icons and set a new beautiful gradient color to the text

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,15 +1,18 @@
-import React, {useRef, useEffect} from 'react';
+import React, { useRef, useEffect } from 'react';
 import Typed from 'typed.js';
-import { AiOutlineMail } from 'react-icons/ai';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import {FaGithub, FaInstagram, FaLinkedinIn, FaTwitch} from 'react-icons/fa';
+import { BsYoutube } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn, FaTwitch } from 'react-icons/fa';
 
 const Main = () => {
   const el = useRef(null);
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['Front-End Developer', 'Back-End Developer', 'Full-Stack Developer'],
+      strings: [
+        'Front-End Developer',
+        'Back-End Developer',
+        'Full-Stack Developer',
+      ],
       startDelay: 300,
       typeSpeed: 150,
       backSpeed: 50,
@@ -33,9 +36,14 @@ const Main = () => {
             Let&apos;s build something together
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hi, I&apos;m <span className='text-[#5651e5]'>Jordan</span>
+            Hi, I&apos;m{' '}
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+              Jordan Salazar
+            </span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A <span ref={el}></span></h1>
+          <h1 className='py-2 text-gray-700'>
+            A <span ref={el}></span>
+          </h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
             I&apos;m a full-stack web developer specializing in building (and
             occasionally designing) exceptional digital experiences. Currently
@@ -51,10 +59,10 @@ const Main = () => {
               <FaGithub />
             </div>
             <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-              <FaInstagram />
+              <FaTwitch />
             </div>
             <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-              <FaTwitch />
+              <BsYoutube />
             </div>
           </div>
         </div>
