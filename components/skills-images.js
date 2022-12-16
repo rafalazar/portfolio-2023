448c62f0ce047bd625aaa914c64cf1ac43d3e81d
Rafael Salazar <jcr.salazar@outlook.com>
2022-12-16 15:38:59 -0500

    Create ES6 re-export default import

---
+++ components/skills-images.js
@@ -0,0 +1,8 @@
+export { default as htmlSkill } from '../public/static/images/html.png';
+export { default as cssSkill } from '../public/static/images/css.png';
+export { default as jsSkill } from '../public/static/images/javascript.png';
+export { default as nodeSkill } from '../public/static/images/node.png';
+export { default as reactSkill } from '../public/static/images/react.png';
+export { default as nextSkill } from '../public/static/images/nextjs.png';
+export { default as tsSkill } from '../public/static/images/typescript.png';
+export { default as tailwindSkill } from '../public/static/images/tailwind.png';
