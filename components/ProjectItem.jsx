e6e2d3ada33ea307b6b04cdcf9853982dcb1d641
Rafael Salazar <jcr.salazar@outlook.com>
2022-12-16 14:15:48 -0500

    feat: refactor code in order to reduce amount of unnecessary code

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { BsGithub } from 'react-icons/bs';
+import { BiLinkExternal } from 'react-icons/bi';
 
-const ProjectItem = ({ title, backgroundImage, stack, projectUrl }) => {
+const ProjectItem = ({ title, backgroundImage, stack, projectUrl, projectRepo }) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
       <Image
@@ -15,11 +17,18 @@ const ProjectItem = ({ title, backgroundImage, stack, projectUrl }) => {
           {title}
         </h3>
         <p className='pb-4 pt-2 text-white text-center'>{stack}</p>
-        <Link href={projectUrl}>
-          <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
-            More info...
-          </p>
-        </Link>
+        <div className='flex items-center justify-evenly'>
+          <Link href={projectUrl} target='_blank'>
+            <p className='text-center px-2 py-2 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
+              <BiLinkExternal />
+            </p>
+          </Link>
+          <Link href={projectRepo} target='_blank'>
+            <p className='text-center px-2 py-2 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
+              <BsGithub />
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import disneyImg from '../public/assets/projects/disney-project.jpg';
-import netflixImg from '../public/assets/projects/netflix-project.jpg';
 import amazonImg from '../public/assets/projects/amazon-project.jpg';
+import netflixImg from '../public/assets/projects/netflix-project.jpg';
 import ProjectItem from './ProjectItem';
 
 const Projects = () => {
+
+  const projects = [
+    {id: 1, title: 'Disney + Clone', backgroundImg: disneyImg, stack: ['React, Firebase, Styled-Components'], projectUrl: 'https://disneyplus-clone-8b014.web.app/', projectRepo: 'https://github.com/rafalazar/disney-plus-clone'},
+    {id: 2, title: 'Amazon Clone', backgroundImg: amazonImg, stack: ['React, Redux, Firebase'], projectUrl: 'https://clone-3ae96.web.app/', projectRepo: 'https://github.com/rafalazar/amazon-clone'},
+    {id: 3, title: 'Netflix Clone', backgroundImg: netflixImg, stack: ['React, Axios, TheMovie API'], projectUrl: 'https://netflix-clone-fc1ec.web.app/', projectRepo: 'https://github.com/rafalazar/netflix-clone'}
+  ]
+
   return (
     <div id='projects' className='w-full'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
@@ -13,24 +20,18 @@ const Projects = () => {
         </p>
         <h2 className='py-4'>What I&apos;ve built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-          <ProjectItem
-            title='Disney + Clone'
-            backgroundImage={disneyImg}
-            stack='React JS'
-            projectUrl='/disney'
-          />
-          <ProjectItem
-            title='Netflix Clone'
-            backgroundImage={netflixImg}
-            stack='React JS'
-            projectUrl='/netflix'
-          />
-          <ProjectItem
-            title='Amazon Clone'
-            backgroundImage={amazonImg}
-            stack='React JS'
-            projectUrl='/amazon'
-          />
+          {
+            projects.map(i => (
+              <ProjectItem
+                key={i['id']}
+                title={i['title']}
+                backgroundImage={i['backgroundImg']}
+                stack={i['stack']}
+                projectUrl={i['projectUrl']}
+                projectRepo={i['projectRepo']}
+              />
+            ))
+          }
         </div>
       </div>
     </div>
